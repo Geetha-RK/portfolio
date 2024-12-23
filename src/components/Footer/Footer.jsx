aa570982edba72cb22c11db3e119207eadc00b26
Geetha R Kumar <geetha2work@gmail.com>
2024-12-23 13:31:08 -0500

    Build the modal component for credits button

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Footer.scss'
 import { assets } from "../assets";
 
 const Footer = () => {
+    const [isModalOpen, setIsModalOpen] = useState(false); 
+
+    const toggleModal = () => {
+        setIsModalOpen(!isModalOpen);
+      };
+
   return (
     <div className='footer'>
         <section className="box1">
@@ -12,10 +18,21 @@ const Footer = () => {
                     <img  className="footer__img" src={assets.logo} alt="logo" />
             </div>
             <div className='footer__credits'>
-                <button className='footer__button'>Credits</button>
+                <button className='footer__button'  onClick={toggleModal}>Credits</button>
             </div>
             </div>
+            <div className='footer__para'>This project was created with love by Geetha Rashinkar Kumar ❤️❤️❤️</div>
         </div>
+        
+        {isModalOpen && (
+          <div className="modal-overlay">
+            <div className="modal-content">
+              <h2>Credits</h2>
+              <p>This project was created with love by [Your Name].</p>
+              <button onClick={toggleModal} className="modal-close-btn">Close</button>
+            </div>
+          </div>
+        )}
         
         </section>
     </div>
